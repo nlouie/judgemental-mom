e1952ed53a704fc1e66a88bcefff7f21ed5f1dd1
nlouie <nlouie@bu.edu>
2016-11-05 16:12:20 -0400

    sry... reset the key.

--- auth_config.py
+++ auth_config.py
@@ -18,8 +18,8 @@ CONFIG = {
         'class_': oauth2.Facebook,
 
         # Facebook is an AuthorizationProvider too.
-        'consumer_key': '1399636540049795',
-        'consumer_secret': 'f4bb3abe109d0af37f4d3d064494b907',
+        'consumer_key': load_json()['facebook']['app_id'],
+        'consumer_secret': load_json()['facebook']['app_secret'],
 
         # But it is also an OAuth 2.0 provider and it needs scope.
         'scope': ['user_about_me', 'email', 'user_posts, user_likes']
