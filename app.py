5f6b7dd3ed6124d1815cca1463d90722117ff53d
nlouie <nlouie@bu.edu>
2016-11-14 18:57:10 -0500

    remove basic authentication in favor for facebook login.

--- app.py
+++ app.py
@@ -20,8 +20,6 @@ from authomatic import Authomatic
 # http://peterhudec.github.io/authomatic/reference/adapters.html#authomatic.adapters.WerkzeugAdapter
 from authomatic.adapters import WerkzeugAdapter
 
-from functools import wraps     # for basic auth
-
 from src import *
 
 # ----------------- Init ----------------------------#
@@ -31,61 +29,19 @@ app = Flask(__name__)
 # Instantiate Authomatic.
 authomatic = Authomatic(CONFIG, 'your secret string', report_errors=False)
 
-# ----------------- Basic Authentication ------------------------#
-
-# need a better authentication method
-
-
-def check_auth(username, password):
-    """This function is called to check if a username /
-    password combination is valid.
-    """
-    return username == 'admin' and password == 'secret'
-
-
-def authenticate():
-    """Sends a 401 response that enables basic auth"""
-    return Response(
-        'Could not verify your access level for that URL.\n'
-        'You have to login with proper credentials', 401,
-        {'WWW-Authenticate': 'Basic realm="Login Required"'})
-
-
-def requires_auth(f):
-    @wraps(f)
-    def decorated(*args, **kwargs):
-        auth = request.authorization
-        if not auth or not check_auth(auth.username, auth.password):
-            return authenticate()
-        return f(*args, **kwargs)
-    return decorated
 
 # -------------------- FUNCTIONS ----------------------------#
-
-
-
 
 
 # ------------------ Routes --------------------------#
 
 # index
-
-
 @app.route('/')
 def hello_world():
     return render_template('index.html')
 
-# facebook login with authomatic
 
-# @app.route('/login', methods=['GET','POST'])
-# def view_login():
-#     if request.method == 'POST':
-#         username = request.form['username']
-#         # plz do this better with hashes, this is just an example
-#         password = request.form['password']
-#         return render_template('login.html', username=username, password=password)
-#     else:
-#         return render_template('login.html')
+# facebook login with authomatic
 
 
 @app.route('/login/<provider_name>/', methods=['GET', 'POST'])
@@ -121,13 +77,6 @@ def login_oauth(provider_name):
 def logout():
     pass
 
-# example of requiring authentication
-
-
-@app.route('/account')
-@requires_auth
-def secret_page():
-    return render_template('account.html')
 
 # analyze
 
