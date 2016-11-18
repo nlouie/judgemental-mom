5463c90d24c20cbbbd0d34961b90af50e1267d7f
Corey Clemente <cclemente@esperdyne.com>
2016-11-17 23:27:23 -0500

    quick tweak

--- app.py
+++ app.py
@@ -79,7 +79,6 @@ def login_oauth(provider_name):
             # We need to update the user to get more info.
             result.user.update()
 
-<<<<<<< Updated upstream
         # first, check if user exists already
         # if not, register the user with result.id
         # else, welcome the user
@@ -87,10 +86,6 @@ def login_oauth(provider_name):
 
         output = extract_facebook(result)
 
-=======
-        output = extract_facebook(result)
-
->>>>>>> Stashed changes
         return render_template('login.html', result=result, output=output)
 
     # Don't forget to return the response.
