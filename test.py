d3d71d4ce3b131cd6db25239c8e1d53581eda005
nlouie <nlouie@bu.edu>
2016-12-01 00:53:18 -0500

    testing comment. look at test.py for an example of database usage....

--- test.py
+++ test.py
@@ -26,3 +26,12 @@ def test_me(s, api_key):
     return r.json()
 
 # eof
+
+
+import user_database
+#
+# db = user_database.get_db()
+# user_database.create_account('1234', 'bob', 'bob@bon.com')
+# all_users = user_database.select_all_users()
+# for user in all_users:
+#     print(user)
